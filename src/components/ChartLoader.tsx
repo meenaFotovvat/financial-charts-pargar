23ab422e76ea88c04836e1b1c485a30be3adda5e
meenaFotovvat <meenafotovvat@gmail.com>
2025-05-07 13:07:27 +0330

    create loading component

---
+++ src/components/ChartLoader.tsx
@@ -0,0 +1,7 @@
+export default function ChartLoader() {
+  return (
+    <div className="w-full h-[300px] flex items-center justify-center text-sm text-gray-400">
+      در حال بارگذاری نمودار...
+    </div>
+  );
+}
